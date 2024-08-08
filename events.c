c0162efed2ab19534b73363f42119bccdc3204f7
Wouter Pepping <wpepping@c3a4c6.42berlin.de>
2024-08-08 18:19:42 +0200

    .

--- events.c
+++ events.c
@@ -6,7 +6,7 @@
 /*   By: wpepping <wpepping@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 19:01:31 by wpepping          #+#    #+#             */
-/*   Updated: 2024/08/08 15:44:42 by wpepping         ###   ########.fr       */
+/*   Updated: 2024/08/08 18:17:57 by wpepping         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,13 +27,13 @@ int	handle_input(int keycode, t_data *data)
 {
 	if (keycode == XK_Escape)
 		mlx_loop_end(data->mlx);
-	if (keycode == XK_w || keycode == XK_W)
+	if (keycode == XK_w || keycode == XK_W || keycode == XK_Up)
 		move_player(data, 0, -1);
-	if (keycode == XK_s || keycode == XK_S)
+	if (keycode == XK_s || keycode == XK_S || keycode == XK_Down)
 		move_player(data, 0, 1);
-	if (keycode == XK_a || keycode == XK_A)
+	if (keycode == XK_a || keycode == XK_A || keycode == XK_Left)
 		move_player(data, -1, 0);
-	if (keycode == XK_d || keycode == XK_D)
+	if (keycode == XK_d || keycode == XK_D || keycode == XK_Right)
 		move_player(data, 1, 0);
 	return (0);
 }
